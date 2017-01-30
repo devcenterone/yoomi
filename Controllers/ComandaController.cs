2033ffc2b4ce16e6cea310d31721904ae18760e0
devcenterone <barbu_sergiu@yahoo.com>
2017-01-30 15:48:06 +0200

    test email

---
+++ Controllers/ComandaController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MailKit.Net.Smtp;
+using MimeKit;
+using MailKit.Security;
+
+
+namespace Yoomi.Controllers
+{
+    public class ComandaController : Controller
+    {
+        private const string fromPassword = "fromPassword";
+        private const string subject = "Subject";
+        private const string body = "Body";
+
+        public IActionResult Index()
+        {            
+            return View();
+        }        
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+
+        public async Task<JsonResult> SendOrder(string producIds)
+        {
+            await SendEmailAsync("sergiu.barbu@gmail.com");
+
+            return Json(new { Result = "OK"});
+        }
+
+
+        public async Task SendEmailAsync(string email, string subject="test", string message="test")
+        {
+            
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Joe Bloggs", "sergiu.barbu@gmail.com"));
+            emailMessage.To.Add(new MailboxAddress("sergiu.barbu@gmail.com", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart("plain") { Text = message };
+            
+
+            using (var client = new SmtpClient())
+            {
+                client.LocalDomain = "yoomi.herokuapp.com";                
+                await client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.None).ConfigureAwait(false);
+                await client.AuthenticateAsync("sergiu.barbu@gmail.com", "smarandaareochiiverzi")
+                       .ConfigureAwait(false);
+                await client.SendAsync(emailMessage).ConfigureAwait(false);
+                await client.DisconnectAsync(true).ConfigureAwait(false);
+            }
+        }
+
+
+
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MailKit.Security;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
 
 namespace Yoomi.Controllers
 {
@@ -13,9 +16,9 @@ namespace Yoomi.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Comanda()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Pagina de comanda.";
 
             return View();
         }
@@ -31,5 +34,49 @@ namespace Yoomi.Controllers
         {
             return View();
         }
+
+
+
+        public async Task<JsonResult> SendOrder(string producIds)
+        {
+            await SendEmailAsync("sergiu.barbu@gmail.com");
+
+            return Json(new { Result = "OK" });
+        }
+
+
+        public async Task SendEmailAsync(string email, string subject = "test", string message = "test")
+        {
+
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Joe Bloggs", "sergiu.barbu@gmail.com"));
+            emailMessage.To.Add(new MailboxAddress("sergiu.barbu@aegon.ro", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart("plain") { Text = message };
+
+
+            using (var client = new SmtpClient())
+            {
+                //client.LocalDomain = "yoomi.herokuapp.com";
+                //client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls)
+                //await client.AuthenticateAsync("sergiu.barbu@gmail.com", "smarandaareochiiverzi")
+                //       .ConfigureAwait(false);
+                //await client.SendAsync(emailMessage).ConfigureAwait(false);
+                //await client.DisconnectAsync(true).ConfigureAwait(false);
+
+
+                await client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.None).ConfigureAwait(false);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                await client.AuthenticateAsync("sergiu.barbu@gmail.com", "smarandaareochiiverzi");
+
+                await client.SendAsync(emailMessage).ConfigureAwait(false);
+                await client.DisconnectAsync(true);
+                await client.DisconnectAsync(true).ConfigureAwait(false);
+            }
+        
+        }
+
+
     }
 }
